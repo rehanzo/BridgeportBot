f704435bac5f6ab86682af4ebffdbf97cb2f5efe
Rehan Rana <rehanalirana@tuta.io>
2022-12-25 23:29:25 -0500

    adds gpt functionality

--- main.py
+++ main.py
@@ -1,18 +1,19 @@
 from fbchat import log, Client, MessageReaction
 from fbchat.models import *
+from chatgpt import *
 import dotenv
 import os
 import db
 import json
+import time
 
-def gptResponse(query):
-    return {"message" : "GPT functionality not supported as of yet."}
-
+chat = None
 # Subclass fbchat.Client and override required methods
 class EchoBot(Client):
     def onMessage(self, author_id, message_object, thread_id, thread_type, **kwargs):
         self.markAsDelivered(thread_id, message_object.uid)
         self.markAsRead(thread_id)
+        global chat
 
         log.info("{} from {} in {}".format(message_object, thread_id, thread_type.name))
         # client.reactToMessage(message_object.uid, MessageReaction.LOVE)
@@ -23,8 +24,9 @@ class EchoBot(Client):
             #    client.reactToMessage(message_object.uid, MessageReaction.LOVE)
             # self.send(message_object, thread_id=thread_id, thread_type=thread_type)
             message = message_object.text
+            words = message.split()
+
             if message.startswith("!notes set"):
-                words = message.split()
                 words.pop(0)
                 words.pop(0)
 
@@ -32,20 +34,21 @@ class EchoBot(Client):
                 note_content = " ".join(words)
                 db.save(note_name, note_content, "notes.sqlite3")
                 self.send(Message(text=(note_name + " has been set.")), thread_id=thread_id, thread_type=thread_type)
+
             elif message.startswith("!notes get"):
-                words = message.split()
                 words.pop(0)
                 words.pop(0)
 
                 note_name = words.pop(0)
                 send = db.load(note_name, "notes.sqlite3")
                 self.send(Message(text=send), thread_id=thread_id, thread_type=thread_type)
+
             elif message.startswith("!notes list"):
                 send = db.keysList("notes.sqlite3")
                 send = "Notes:\n" + send
                 self.send(Message(text=send), thread_id=thread_id, thread_type=thread_type)
+
             elif message.startswith("!notes clear"):
-                words = message.split()
                 words.pop(0)
                 words.pop(0)
 
@@ -53,13 +56,25 @@ class EchoBot(Client):
                 db.clear(note_name, "notes.sqlite3")
 
                 self.send(Message(text=(note_name + " has been cleared.")), thread_id=thread_id, thread_type=thread_type)
+
             elif message.startswith("!gpt"):
-                words = message.split()
                 words.pop(0)
 
+                if chat == None:
+                    chat = ChatGPT()
+
                 query = " ".join(words)
-                response = gptResponse(query)
-                self.send(Message(text=response["message"]), thread_id=thread_id, thread_type=thread_type)
+                response = chat.gptResponse(query)
+                self.send(Message(text=response), thread_id=thread_id, thread_type=thread_type)
+
+            elif message.startswith("!timeout"):
+                thread = self.fetchThreadInfo(thread_id)[thread_id]
+                print("thread's name: {}".format(thread.name))
+                print("thread's type: {}".format(thread.type))
+
+                print(thread)
+                users = self.fetchAllUsersFromThreads([thread])
+                print(users)
 
 
 cookies = {}
